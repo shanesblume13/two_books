0ecf2e959ca46fcdb1f73b974652c974008266de
Shane Blume <shanesblume@gmail.com>
2022-04-11 15:59:30 -0400

    Using heroes to transition between book collection and book details.

--- lib/screens/books_screen.dart
+++ lib/screens/books_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:two_books/bloc/get_books_bloc.dart';
+import 'package:two_books/bloc/show_scroll_to_top_bloc.dart';
+import 'package:two_books/models/book.dart';
 import 'package:two_books/models/book_response.dart';
 
 class BooksScreen extends StatefulWidget {
@@ -33,12 +35,33 @@ class _BooksScreenState extends State<BooksScreen> {
             return Text(snapshot.data!.error);
           }
           return ListView.builder(
+            controller: showScrollToTopBloc.scrollController,
             itemCount: snapshot.data!.books.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(snapshot.data!.books[index].title),
-                subtitle: Text(
-                  'by ${snapshot.data!.books[index].author.name}',
+              Book book = snapshot.data!.books[index];
+
+              return Hero(
+                tag: book.id,
+                transitionOnUserGestures: true,
+                child: Material(
+                  child: ListTile(
+                    onTap: () => _goToBookDetailsPage(context, book),
+                    leading: CircleAvatar(
+                      backgroundColor: book.color,
+                      child: const Icon(
+                        Icons.book,
+                        color: Colors.white,
+                      ),
+                    ),
+                    title: Text(book.title),
+                    subtitle: Text(
+                      'by ${book.author.name}',
+                    ),
+                    trailing: const Icon(
+                      Icons.keyboard_arrow_right,
+                      color: Colors.grey,
+                    ),
+                  ),
                 ),
               );
             },
@@ -49,5 +72,46 @@ class _BooksScreenState extends State<BooksScreen> {
         );
       },
     );
+  }
+
+  void _goToBookDetailsPage(BuildContext context, Book book) {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text('Book Details'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Hero(
+              tag: book.id,
+              transitionOnUserGestures: true,
+              child: Material(
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: book.color,
+                    child: const Icon(
+                      Icons.book,
+                      color: Colors.white,
+                    ),
+                  ),
+                  title: Text(book.title),
+                  subtitle: Text(
+                    'by ${book.author.name}',
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: const Icon(Icons.star),
+                title: Text('Reviews'),
+                subtitle: Text('${book.reviews.length} reviews'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
