d0cb618f8e1b6446e361ea3497d8f0d252727ace
Shane Blume <shanesblume@gmail.com>
2022-04-11 10:17:18 -0400

    Adding authors to books.

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 A books app with a Collection page (index) and Details page using https://thomaxxl.pythonanywhere.com API. Built with Flutter.
 ## Packages
 
-The api follows the Json:API standard. The https://pub.dev/packages/japx package is used to flatten complex json objects and relationships into simple Map<String, dynamic> objects.
+The api follows the Json:API standard. The https://pub.dev/packages/japx package is used to flatten complex json objects and relationships into simple Map<String, dynamic> objects. The api respository methods that fetch data will include relationships and use Japx.decode to flattern those relationships before passing them into 
+the fromJson model methods.
 
 Dio
 
@@ -16,6 +17,7 @@ Dio
 Testing
 Environments
 Data Sources - Repository wrapper and subclasses for api/db/etc.
+Dio and request logging and interceptors
 UI/UX/Branding
 Logging for debug environements
 Navigation in app bar
@@ -25,3 +27,5 @@ Details Page
 Search function
 Theme
 Splash Screen
+Dependency Injection
+Add pull to refresh
--- lib/screens/books_screen.dart
+++ lib/screens/books_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:two_books/bloc/get_books_bloc.dart';
+import 'package:two_books/models/book_response.dart';
 
 class BooksScreen extends StatefulWidget {
   const BooksScreen({Key? key}) : super(key: key);
@@ -9,7 +11,43 @@ class BooksScreen extends StatefulWidget {
 
 class _BooksScreenState extends State<BooksScreen> {
   @override
+  void initState() {
+    super.initState();
+    getBooksBloc.getBooks();
+  }
+
+  // TODO Do we really need to dispose and drain here?
+  @override
+  void dispose() {
+    getBooksBloc.drainStream();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('Hello World');
+    return StreamBuilder<BookResponse>(
+      stream: getBooksBloc.subject.stream,
+      builder: (context, AsyncSnapshot<BookResponse> snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.data!.error != '') {
+            return Text(snapshot.data!.error);
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.books.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(snapshot.data!.books[index].title),
+                subtitle: Text(
+                  'by ${snapshot.data!.books[index].author.name}',
+                ),
+              );
+            },
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
