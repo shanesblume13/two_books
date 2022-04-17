87b015bf21977924ffb6faed3a9147a9dda245f0
Shane Blume <shanesblume@gmail.com>
2022-04-16 20:41:27 -0400

    Replacing scrollToTopBloc by making the scroll FAB a stateful widget.

--- lib/widgets/scroll_to_top_fab.dart
+++ lib/widgets/scroll_to_top_fab.dart
@@ -1,28 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:two_books/bloc/scroll_to_top_bloc.dart';
 
-class ScrollToTopFAB extends StatelessWidget {
+class ScrollToTopFAB extends StatefulWidget {
+  final ScrollController scrollController;
+
   const ScrollToTopFAB({
     Key? key,
+    required this.scrollController,
   }) : super(key: key);
 
   @override
+  State<ScrollToTopFAB> createState() => _ScrollToTopFABState();
+}
+
+class _ScrollToTopFABState extends State<ScrollToTopFAB> {
+  bool showFAB = false;
+
+  iinitState() {
+    super.initState();
+  }
+
+  // Scroll page to top w/ shared scroll controller
+  void scrollToTop() {
+    if (widget.scrollController.hasClients) {
+      widget.scrollController.animateTo(0,
+          duration: const Duration(seconds: 1), curve: Curves.linear);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return StreamBuilder<bool>(
-      stream: scrollToTopBloc.showFABSubject.stream,
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data == true) {
-          return FloatingActionButton(
+    widget.scrollController.addListener(
+      () {
+        if (widget.scrollController.offset >= 400) {
+          setState(() {
+            showFAB = true;
+          });
+        } else {
+          setState(() {
+            showFAB = false;
+          });
+        }
+      },
+    );
+
+    return showFAB == true
+        ? FloatingActionButton(
             backgroundColor: Theme.of(context).colorScheme.secondary,
-            onPressed: () => scrollToTopBloc.scrollToTop(),
+            onPressed: () => scrollToTop(),
             child: const Icon(
               Icons.arrow_upward,
               color: Colors.white,
             ),
-          );
-        }
-        return Container();
-      },
-    );
+          )
+        : Container();
   }
 }
